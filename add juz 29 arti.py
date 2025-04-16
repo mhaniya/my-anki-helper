787acd908aefc935b984865466f102542b9db09d
unknown <hanifadzkiya@gmail.com>
2025-04-16 17:44:00 +0700

    Generate cards for juz 29 Arti per surah

--- add juz 29 arti.py
+++ add juz 29 arti.py
@@ -19,5 +19,39 @@ def get_ayat(no_quran, no_ayat):
     data = res.read()
     return json.loads(data.decode("utf-8"))["data"]["text"]
 
-print(get_translation(70, 11))
-print(get_ayat(70, 11))
+def add_notes(surah_name, ayat_number, arabic_text, translate):
+    conn = http.client.HTTPConnection("localhost", 8765)
+    payload = json.dumps({
+    "action": "addNote",
+    "version": 6,
+    "params": {
+        "note": {
+        "deckName": "Juz 29 Arti",
+        "modelName": "Basic",
+        "fields": {
+            "Front": arabic_text,
+            "Back": translate,
+            "Detail": f"{surah_name}: {ayat_number}"
+        },
+        "options": {
+            "allowDuplicate": False,
+            "duplicateScope": "deck"
+        },
+        "tags": [
+            surah_name
+        ]
+        }
+    }
+    })
+    headers = {
+    'Content-Type': 'application/json'
+    }
+    conn.request("POST", "/", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    print(data.decode("utf-8"))
+
+for i in range(12, 45):
+    translate = get_translation(70, i)
+    arabic_text = get_ayat(70, i)
+    add_notes("Al-Maarij", i, arabic_text, translate)
